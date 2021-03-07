583aafa74092962acd4013b72ba222dd0828a7d4
ogataka50 <ogataka50@gmail.com>
2021-03-07 23:10:14 +0900

    Revert "Revert "first commit""
    This reverts commit 3c8791a7178de09137978cc204ade3757a1b9865.

---
+++ src/cats/cats.module.ts
@@ -0,0 +1,13 @@
+import {Module} from '@nestjs/common';
+import {CatsService} from "./cats.service";
+import {CatsController} from "./cats.controller";
+
+
+@Module({
+    controllers: [CatsController],
+    providers: [CatsService],
+    exports: [CatsService],
+})
+export class CatsModule {
+    constructor(private catsService: CatsService) {}
+}
